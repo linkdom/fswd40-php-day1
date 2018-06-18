f1125628723fef2be381c2226b1ba6228d70e8a2
Dominik Link <Vorbild94@gmail.com>
2018-06-18 10:01:38 +0200

    finished Exercise 1

---
+++ Exercise 1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercise 1</title>
+  </head>
+  <body>
+    <?php
+    $myname = "<h1>Dominik</h1>";
+    echo $myname;
+    ?>
+  </body>
+</html>
