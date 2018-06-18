05f7a68a83e780ec6856cb3e5f27582d621ee5f5
Dominik Link <Vorbild94@gmail.com>
2018-06-18 11:07:50 +0200

    finished Exercise 5

--- Exercise 5/index.php
+++ Exercise 5/index.php
@@ -2,9 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercise 5</title>
   </head>
   <body>
-
+    <?php
+    $array = array(1,2,3,4,5,6,7,8,9,10);
+    foreach ($array as $value) {
+      echo "Value is" . $value ."</br>";
+    }
+     ?>
   </body>
 </html>
